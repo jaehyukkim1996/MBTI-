4dede21c21eacea27034f8e29e0ff72533414ab6
Co-Ji <100311029+Co-Ji@users.noreply.github.com>
2022-02-26 02:26:05 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 client = MongoClient(
+    '')
 db = client.dblewigolski
 
 app = Flask(__name__)
@@ -288,4 +289,5 @@ def celeb():
 
 # 포트는 5000으로 설정함 
 if __name__ == '__main__':
+    app.run('0.0.0.0', port=5003, debug=True)
 
